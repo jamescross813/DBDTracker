8bc1f25927532921072c0c3777c87bd894644f45
jamescross <crossjames813@gmail.com>
2021-04-29 13:49:01 -0500

    set up login route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,4 +39,16 @@ class ApplicationController < Sinatra::Base
     get '/login' do
         erb :'/sessions/login'
     end
+
+    post '/login' do
+        binding.pry
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/failure'
+        end
+    end
+
 end
