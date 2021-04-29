3cb001cd6184a6c15ff1fb07e4ebdcb66c1bf106
jamescross <crossjames813@gmail.com>
2021-04-29 16:21:56 -0500

    moved help folder

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require './app/controllers/application_controller'
+require_all './app/controllers'
 require_all './app/models'
 
 ActiveRecord::Base.establish_connection(
